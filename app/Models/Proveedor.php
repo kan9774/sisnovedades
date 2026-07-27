8ca2432ff6aedca1c32b176e2240931ec5e5a002
kan9774 <kan9774@hotmail.com>
2026-07-26 22:48:23 -0300

    22:48

--- app/Models/Proveedor.php
+++ app/Models/Proveedor.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Proveedor extends Model
 {
     use HasFactory;
-
+    protected $table = 'proveedores';
     protected $fillable = [
         'nombre',
         'contacto',
